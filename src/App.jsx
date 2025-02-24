3b78dc7ba025b3e4f36937182d20ad5c9d2dc46c
Fede <fede.atzeni372@gmail.com>
2025-02-24 15:51:15 +0100

    feat routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header.jsx"
+
+import HomePage from "./pages/HomePage.jsx"
+import AboutPage from "./pages/AboutPage.jsx"
+import PostPage from "./pages/PostPage.jsx"
+
 function App() {
 
   return (
     <>
-      <h1>Titolo</h1>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"  element={<HomePage />} ></Route>
+          <Route path="/about_us"  element={<AboutPage />} ></Route>
+          <Route path="/posts"  element={<PostPage />} ></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const navLinks = [
+    { id: 1, text: "Home", url: "/" },
+    { id: 2, text: "Chi Siamo", url: "/about_us" },
+    { id: 3, text: "Posts", url: "/posts" },
+]
+
+export default function Header() {
+
+    return (
+        <header>
+
+        </header>
+    )
+}
